e3977e20888e24cf176e427124a01599044d3551
ajuan92 <ajuan.carlos626@gmail.com>
2021-05-22 21:51:50 -0500

    GUI state machin
    Primera parte completa hay comunicación con el bloque de control a través de la interfaz gráfica, falta optimizar aplicación

--- Graphic_App/GUI_Control.py
+++ Graphic_App/GUI_Control.py
@@ -12,13 +12,14 @@ import struct
 import multiprocessing as mprocess
 from multiprocessing import Array
 
+
 CURR_PATH = os.path.dirname(os.path.realpath(__file__))
 LOW_DRIVE_PATH = CURR_PATH + "\\..\\Control_Block"
 
 sys.path.append(
     LOW_DRIVE_PATH)
 
-import PEATC_Control as Ctrl
+from PEATC_Control import PEATC_Control
 
 PEATC_CONFIG_SAMPLE_WAIT_TIME = 10
 PEATC_CONFIG_DIAG_WAIT_TIME = 10
@@ -62,52 +63,49 @@ class GUI_Control():
         '''!
         Inicializa la maquina de estados para realizar la prueba de PEATC
         '''
+        self.__Cmd_Template = Array('i', range(5))
 
-        self.__Cmd_Template = {
-            "Cmd": 0,
-            "Gs_SignaldB": 0,
-            "Gs_Latency": 0,
-            "Gs_Polarity": 0,
-            "Gs_Freq": 0,
-        }
+        self.GuiCurrState = Array('i', range(1))
+        self.GuiPrevState = Array('i', range(1))
 
-        self.__Ctrl_Cmd_output, self.__Ctrl_Cmd_input = mprocess.Pipe(False)
-        self.__Ctrl_Result_output, self.__Ctrl_Result_input = mprocess.Pipe(
+        self.Ctrl_Cmd_output, self.Ctrl_Cmd_input = mprocess.Pipe(False)
+        self.Ctrl_Result_output, self.Ctrl_Result_input = mprocess.Pipe(
             False)
 
-        self.__Ctrl_State = Array('i', range(1))
-
-        self.__Diag_Table_output, self.__Diag_Table_input = mprocess.Pipe(
+        self.Diag_Table_output, self.Diag_Table_input = mprocess.Pipe(
             False)
-        self.__Diag_Result_output, self.__Diag_Result_input = mprocess.Pipe(
+        self.Diag_Result_output, self.Diag_Result_input = mprocess.Pipe(
             False)
 
-        self.__Diag_State = Array('i', range(1))
+        self.Ctrl_State = Array('i', range(1))
 
-        self.__Ctrl_Block = Ctrl.PEATC_Control()
+        self.Diag_State = Array('i', range(1))
 
-        self.__Ctrl_Task = mprocess.Process(target=self.__Ctrl_Block.ControlHandler, args=(
-            self.__Ctrl_Cmd_output, self.__Ctrl_Result_input, self.__Ctrl_State),)
+        Ctrl_Block = PEATC_Control()
 
-        self.__Diag_Task = mprocess.Process(target=self.__Ctrl_Block.DiagHandler, args=(
-            self.__Diag_Table_output, self.__Diag_Result_input, self.__Diag_State),)
+        Ctrl_Task = mprocess.Process(target=Ctrl_Block.ControlHandler, args=(
+            self.Ctrl_Cmd_output, self.Ctrl_Result_input, self.Ctrl_State),)
 
-        self.__Gui_Task = mprocess.Process(target=self.GuiCom)
+        Diag_Task = mprocess.Process(target=Ctrl_Block.DiagHandler, args=(
+            self.Diag_Table_output, self.Diag_Result_input, self.Diag_State),)
 
-        self.__Ctrl_Task.daemon = True
-        self.__Ctrl_Task.start()
+        Ctrl_Task.daemon = True
+        Ctrl_Task.start()
 
-        self.__Diag_Task.daemon = True
-        self.__Diag_Task.start()
+        Diag_Task.daemon = True
+        Diag_Task.start()
 
-        self.__Gui_Task.daemon = True
-        self.__Gui_Task.start()
+        Gui_Task = mprocess.Process(target=self.GuiCom)
 
-        self.__InitGUI()
+        Gui_Task.daemon = True
+        Gui_Task.start()
+
+        self.InitGUI()
 
         print("Inicialización finalizada")
+        print(self.GuiCurrState[0])
 
-    def __InitGUI(self):
+    def InitGUI(self):
         '''!
         Reporta el estado
         '''
@@ -186,52 +184,64 @@ class GUI_Control():
         self.Entry_Freq.current(0)
 
     def __clicked(self):
-        print("Inicio Click")
-        self.__Cmd_Template["Cmd"] = 1
-        self.__Cmd_Template["Gs_SignaldB"] = self.Entry_SignaldB.get()
-        self.__Cmd_Template["Gs_Latency"] = self.Entry_Latency.get()
-        self.__Cmd_Template["Gs_Polarity"] = self.Entry_Polarity.get()
-        self.__Cmd_Template["Gs_Freq"] = self.Entry_Freq.get()
+
+        if self.GuiCurrState[0] is 0:
+            print("Inicio Click")
+            sys.stdout.flush()
+            self.__Cmd_Template[0] = 1
+            self.__Cmd_Template[1] = self.Entry_SignaldB.get()
+            self.__Cmd_Template[2] = self.Entry_Latency.get()
+            self.__Cmd_Template[3] = self.Entry_Polarity.get()
+            self.__Cmd_Template[4] = self.Entry_Freq.get()
 
     def GuiCom(self):
 
         print("Inicio Tarea de Comunicación")
         sys.stdout.flush()
 
-        GuiCurrState = 0
-
         while True:
 
-            if GuiCurrState is 0:
+            if self.GuiPrevState[0] is not self.GuiCurrState[0]:
+                self.GuiPrevState[0] = self.GuiCurrState[0]
+                print(self.GuiCurrState[0])
+                sys.stdout.flush()
 
-                if self.__Cmd_Template["Cmd"] is 1:
+            if self.GuiCurrState[0] is 0:
 
-                    CtrlCurrState = self.__Ctrl_State[0]
+                if self.__Cmd_Template[0] is 1:
+                    print("dsds")
+                    CtrlCurrState = self.Ctrl_State[0]
                     print(CtrlCurrState)
+
+                    Cmd_Template["Cmd"] = self.__Cmd_Template[0]
+                    Cmd_Template["Gs_SignaldB"] = self.__Cmd_Template[1]
+                    Cmd_Template["Gs_Latency"] = self.__Cmd_Template[2]
+                    Cmd_Template["Gs_Polarity"] = self.__Cmd_Template[3]
+                    Cmd_Template["Gs_Freq"] = self.__Cmd_Template[4]
 
                     if CtrlCurrState == 0:
                         print("Envio Conf")
-                        self.__Ctrl_Cmd_input.send(self.__Cmd_Template)
-                        self.__Cmd_Template["Cmd"] = 0
+                        self.Ctrl_Cmd_input.send(Cmd_Template)
+                        self.__Cmd_Template[0] = 0
                         print("Fin Conf")
-                        GuiCurrState = 1
+                        self.GuiCurrState[0] = 1
 
-            elif GuiCurrState is 1:
+            elif self.GuiCurrState[0] is 1:
 
-                CtrlCurrState = self.__Ctrl_State[0]
+                CtrlCurrState = self.Ctrl_State[0]
                 print(CtrlCurrState)
 
                 if CtrlCurrState is 0:
-                    GuiCurrState = 2
+                    self.GuiCurrState[0] = 2
 
-            elif GuiCurrState is 2:
+            elif self.GuiCurrState[0] is 2:
 
                 print("Inicio Reciv Result")
-                WavePEATC1, FullWaveData1 = self.__Ctrl_Result_output.recv()
+                WavePEATC1, FullWaveData1 = self.Ctrl_Result_output.recv()
                 print("Fin Reciv Result")
                 print("---------")
                 print(WavePEATC1)
                 print(FullWaveData1)
                 print("---------")
 
-                GuiCurrState = 0
+                self.GuiCurrState[0] = 0
--- Graphic_App/Init_App.py
+++ Graphic_App/Init_App.py
@@ -1,4 +1,8 @@
 from GUI_Control import GUI_Control
+import multiprocessing as mprocess
+from multiprocessing import Array
+from PEATC_Control import PEATC_Control
+from GUI_Control import GUI_Control
 
 if __name__ == '__main__':
     GUI_Control()
