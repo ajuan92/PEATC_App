78d4443656d419f4caa70d877702e05e93011354
ajuan92 <ajuan.carlos626@gmail.com>
2021-06-13 01:09:01 -0500

    Interfaz grafica
    Se crea comunicación entre proceso que gestiona la prueba y la tarea que despliega las gráficas

--- Graphic_App/GUI_Control.py
+++ Graphic_App/GUI_Control.py
@@ -5,13 +5,17 @@ from tkinter import *
 from tkinter.ttk import Combobox
 from tkinter import ttk
 from tkinter import Label
+from random import randint
 
 from time import sleep
 import struct
 
 import multiprocessing as mprocess
 from multiprocessing import Array
+from multiprocessing import Manager
 
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 
 CURR_PATH = os.path.dirname(os.path.realpath(__file__))
 LOW_DRIVE_PATH = CURR_PATH + "\\..\\Control_Block"
@@ -49,6 +53,11 @@ STATE_INIT_DIAGNOSTIC = 5
 STATE_WAIT_DIAGNOSTIC = 6
 STATE_SEND_RESULT = 7
 
+CONFIG_SignaldB = [30, 40, 50, 60]
+
+CONFIG_Latency = [100, 233, 120, 54]
+CONFIG_Freq = [50, 70, 100, 150]
+
 
 class GUI_Control():
     '''!
@@ -63,10 +72,31 @@ class GUI_Control():
         '''!
         Inicializa la maquina de estados para realizar la prueba de PEATC
         '''
-        self.__Cmd_Template = Array('i', range(5))
+        print('Inicio Manager')
+        manager = Manager()
+        print('Fin Manager')
 
-        self.GuiCurrState = Array('i', range(1))
-        self.GuiPrevState = Array('i', range(1))
+        self.WaveData = manager.list()
+        self.WaveData.append(
+            dict({'SignaldB': 0, 'NewData': 0, 'Wave': [], 'FullSignal': []}))
+        print(self.WaveData)
+
+        for MemIndex in range(len(CONFIG_SignaldB)):
+            Currd = self.WaveData[MemIndex]
+            Currd['SignaldB'] = CONFIG_SignaldB[MemIndex]
+            self.WaveData[MemIndex] = Currd
+
+            if MemIndex is not len(CONFIG_SignaldB) - 1:
+                self.WaveData.append(
+                    {'SignaldB': 0, 'NewData': 0, 'Wave': [], 'FullSignal': []})
+
+        print(self.WaveData)
+
+        self.__Cmd_Template = Array('i', range(5))
+        self.__Cmd_DiagAge = Array('i', range(2))
+
+        self.GuiCurrState = STATE_STAND_BY
+        self.GuiPrevState = STATE_RESET
 
         self.Ctrl_Cmd_output, self.Ctrl_Cmd_input = mprocess.Pipe(False)
         self.Ctrl_Result_output, self.Ctrl_Result_input = mprocess.Pipe(
@@ -103,46 +133,64 @@ class GUI_Control():
         self.InitGUI()
 
         print("Inicialización finalizada")
-        print(self.GuiCurrState[0])
+        print(self.GuiCurrState)
 
     def InitGUI(self):
         '''!
         Reporta el estado
         '''
-        Window = Tk()
-        Window.title("PEATC APP")
-        Window.geometry("1000x600")
+        self.Window = Tk()
+        self.Window.title("PEATC APP")
+        self.Window.geometry("1000x600")
 
-        #------------
-        self.__ConfParam(Window)
-        #------------
+        self.__ConfParam()
 
-        BtnFrame = Frame(Window)
-        BtnFrame.pack(fill="both", side="right", expand=1)
-        BtnFrame.config(bg="white")
-        BtnFrame.config(width="400", height="400")
+        self.BtnFrame = Frame(self.Window)
+        self.BtnFrame.pack(fill="both", side="right", expand=1)
+        self.BtnFrame.config(bg="white")
+        self.BtnFrame.config(width="400", height="400")
 
-        GrafFrame = Frame(Window)
-        GrafFrame.pack(fill="both", side="left", expand=1)
-        GrafFrame.config(bg="white")
-        GrafFrame.config(width="600", height="400")
+        self.GrafFrame = Frame(self.Window)
+        self.GrafFrame.pack(fill="both", side="left", expand=1)
+        self.GrafFrame.config(bg="white")
+        self.GrafFrame.config(width="600", height="400")
 
-        Graf_Notebook = ttk.Notebook(GrafFrame, width="600", height="400")
-        Graf_Notebook.place(x=10, y=1)
-        GrafTab = ttk.Frame(Graf_Notebook)
-        WaveTab = ttk.Frame(Graf_Notebook)
-        Graf_Notebook.add(GrafTab, text='First')
-        Graf_Notebook.add(WaveTab, text='Second')
-        Graf_Notebook.pack(fill='both', expand=True)
+        self.Graf_Notebook = ttk.Notebook(
+            self.GrafFrame, width="600", height="400")
+        self.Graf_Notebook.place(x=10, y=1)
+        self.GrafTab = ttk.Frame(self.Graf_Notebook)
+        self.WaveTab = ttk.Frame(self.Graf_Notebook)
+        self.Graf_Notebook.add(self.GrafTab, text='First')
+        self.Graf_Notebook.add(self.WaveTab, text='Second')
+        self.Graf_Notebook.pack(fill='both', expand=True)
 
-        btn = Button(BtnFrame, text="Click Me", command=self.__clicked)
-        btn.place(bordermode=OUTSIDE, height=80, width=200, x=100, y=200)
+        self.__ConfBottons()
 
-        Window.mainloop()
+        self.fig = Figure(figsize=(5, 5), dpi=100)
 
-    def __ConfParam(self, Window):
+        self.canvas = FigureCanvasTkAgg(self.fig, self.GrafTab)
+        self.canvas.draw()
+        self.canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=True)
+        self.GrafTab.after(1000, self.__UpdateData)
 
-        ConfParamFrame = Frame(Window)
+        self.Window.mainloop()
+
+    def __UpdateData(self):
+
+        print(self.WaveData[0])
+        GrafData = self.WaveData[0]['FullSignal']
+        print(GrafData)
+        self.fig.clear()
+        self.fig.add_subplot(1, 1, 1).plot(
+            list(range(0, len(GrafData))), GrafData)
+        self.fig.tight_layout()
+        self.canvas.draw()
+
+        self.GrafTab.after(1000, self.__UpdateData)
+
+    def __ConfParam(self):
+
+        ConfParamFrame = Frame(self.Window)
         ConfParamFrame.pack(fill="both", side="bottom", expand=1)
         ConfParamFrame.config(bg="white")
         ConfParamFrame.config(width="600", height="50")
@@ -153,7 +201,7 @@ class GUI_Control():
         self.Entry_SignaldB = ttk.Combobox(
             ConfParamFrame, width=5, state='readonly')
         self.Entry_SignaldB.place(x=100, y=70)
-        self.Entry_SignaldB['values'] = [30, 40, 50, 60]
+        self.Entry_SignaldB['values'] = CONFIG_SignaldB
         self.Entry_SignaldB.current(0)
 
         Label_Latency = Label(ConfParamFrame, text="Latency")
@@ -162,7 +210,7 @@ class GUI_Control():
         self.Entry_Latency = ttk.Combobox(
             ConfParamFrame, width=5, state='readonly')
         self.Entry_Latency.place(x=200, y=70)
-        self.Entry_Latency['values'] = [100, 233, 120, 54]
+        self.Entry_Latency['values'] = CONFIG_Latency
         self.Entry_Latency.current(0)
 
         Label_Polarity = Label(ConfParamFrame, text="Polarity")
@@ -180,19 +228,51 @@ class GUI_Control():
         self.Entry_Freq = ttk.Combobox(
             ConfParamFrame, width=5, state='readonly')
         self.Entry_Freq.place(x=400, y=70)
-        self.Entry_Freq['values'] = [50, 70, 100, 150]
+        self.Entry_Freq['values'] = CONFIG_Freq
         self.Entry_Freq.current(0)
 
-    def __clicked(self):
+        Label_Age = Label(ConfParamFrame, text="Age")
+        Label_Age.place(x=500, y=50)
+        Label_Age.config(bg="white")
+        self.Entry_Age = ttk.Combobox(
+            ConfParamFrame, width=5, state='readonly')
+        self.Entry_Age.place(x=500, y=70)
+        self.Entry_Age['values'] = list(range(0, 100))
+        self.Entry_Age.current(0)
 
-        if self.GuiCurrState[0] is 0:
+    def __ConfBottons(self):
+
+        btnGenSignal = Button(self.BtnFrame, text="Get PEATC",
+                              command=self.__GenSignalBotton)
+        btnGenSignal.place(bordermode=OUTSIDE, height=80,
+                           width=200, x=100, y=100)
+
+        btnDiagSignal = Button(self.BtnFrame, text="Diagnostic",
+                               command=self.__GenDiagBotton)
+        btnDiagSignal.place(bordermode=OUTSIDE, height=80,
+                            width=200, x=100, y=300)
+
+    def __GenSignalBotton(self):
+
+        if self.GuiCurrState is STATE_STAND_BY:
             print("Inicio Click")
             sys.stdout.flush()
             self.__Cmd_Template[0] = 1
-            self.__Cmd_Template[1] = self.Entry_SignaldB.get()
-            self.__Cmd_Template[2] = self.Entry_Latency.get()
-            self.__Cmd_Template[3] = self.Entry_Polarity.get()
-            self.__Cmd_Template[4] = self.Entry_Freq.get()
+            self.__Cmd_Template[1] = int(self.Entry_SignaldB.get())
+            self.__Cmd_Template[2] = int(self.Entry_Latency.get())
+            self.__Cmd_Template[3] = int(self.Entry_Polarity.get())
+            self.__Cmd_Template[4] = int(self.Entry_Freq.get())
+
+    def __GenDiagBotton(self):
+
+        if self.GuiCurrState is STATE_STAND_BY:
+            print("Inicio Click DiagBotton")
+            sys.stdout.flush()
+            self.__Cmd_DiagAge[0] = 1
+            self.__Cmd_DiagAge[1] = int(self.Entry_Age.get())
+        else:
+            print("Inicio Click Disable")
+            sys.stdout.flush()
 
     def GuiCom(self):
 
@@ -201,40 +281,48 @@ class GUI_Control():
 
         while True:
 
-            if self.GuiPrevState[0] is not self.GuiCurrState[0]:
-                self.GuiPrevState[0] = self.GuiCurrState[0]
-                print(self.GuiCurrState[0])
+            if self.GuiPrevState is not self.GuiCurrState:
+                self.GuiPrevState = self.GuiCurrState
+                print(self.GuiCurrState)
                 sys.stdout.flush()
 
-            if self.GuiCurrState[0] is 0:
+            if self.GuiCurrState is STATE_STAND_BY:
 
                 if self.__Cmd_Template[0] is 1:
-                    print("dsds")
-                    CtrlCurrState = self.Ctrl_State[0]
-                    print(CtrlCurrState)
+                    print("Read PEATC")
+                    self.GuiCurrState = STATE_INIT_TEST
+                elif self.__Cmd_DiagAge[0] is 1:
+                    self.GuiCurrState = STATE_INIT_DIAGNOSTIC
 
-                    Cmd_Template["Cmd"] = self.__Cmd_Template[0]
-                    Cmd_Template["Gs_SignaldB"] = self.__Cmd_Template[1]
-                    Cmd_Template["Gs_Latency"] = self.__Cmd_Template[2]
-                    Cmd_Template["Gs_Polarity"] = self.__Cmd_Template[3]
-                    Cmd_Template["Gs_Freq"] = self.__Cmd_Template[4]
+            elif self.GuiCurrState is STATE_INIT_TEST:
 
-                    if CtrlCurrState == 0:
-                        print("Envio Conf")
-                        self.Ctrl_Cmd_input.send(Cmd_Template)
-                        self.__Cmd_Template[0] = 0
-                        print("Fin Conf")
-                        self.GuiCurrState[0] = 1
-
-            elif self.GuiCurrState[0] is 1:
+                Cmd_Template["Cmd"] = self.__Cmd_Template[0]
+                Cmd_Template["Gs_SignaldB"] = self.__Cmd_Template[1]
+                Cmd_Template["Gs_Latency"] = self.__Cmd_Template[2]
+                Cmd_Template["Gs_Polarity"] = self.__Cmd_Template[3]
+                Cmd_Template["Gs_Freq"] = self.__Cmd_Template[4]
 
                 CtrlCurrState = self.Ctrl_State[0]
-                print(CtrlCurrState)
+
+                if CtrlCurrState == 0:
+                    print("Envio Conf")
+                    self.Ctrl_Cmd_input.send(Cmd_Template)
+                    self.__Cmd_Template[0] = 0
+                    print("Fin Conf")
+                    self.GuiCurrState = STATE_WAIT_RAW_DATA
+                    print("Waiting Result...")
+                else:
+                    self.GuiCurrState = STATE_STAND_BY
+                    print("Return Stand By")
+
+            elif self.GuiCurrState is STATE_WAIT_RAW_DATA:
+
+                CtrlCurrState = self.Ctrl_State[0]
 
                 if CtrlCurrState is 0:
-                    self.GuiCurrState[0] = 2
+                    self.GuiCurrState = STATE_ANALYZE_DATA
 
-            elif self.GuiCurrState[0] is 2:
+            elif self.GuiCurrState is STATE_ANALYZE_DATA:
 
                 print("Inicio Reciv Result")
                 WavePEATC1, FullWaveData1 = self.Ctrl_Result_output.recv()
@@ -242,6 +330,59 @@ class GUI_Control():
                 print("---------")
                 print(WavePEATC1)
                 print(FullWaveData1)
+                print(Cmd_Template["Gs_SignaldB"])
+
+                for i, dic in enumerate(self.WaveData):
+                    if dic['SignaldB'] == Cmd_Template["Gs_SignaldB"]:
+                        GetPEATCDict = self.WaveData[i]
+                        GetPEATCDict['NewData'] = 1
+                        GetPEATCDict['Wave'] = WavePEATC1
+                        GetPEATCDict['FullSignal'] = FullWaveData1
+                        self.WaveData[i] = GetPEATCDict
+
                 print("---------")
 
-                self.GuiCurrState[0] = 0
+                print(self.WaveData)
+                self.GuiCurrState = STATE_STAND_BY
+
+            elif self.GuiCurrState is STATE_INIT_DIAGNOSTIC:
+
+                self.WaveTable = []
+
+                DiagCurrState = self.Diag_State[0]
+
+                if DiagCurrState == 0:
+                    # Cuantas señales SON NECESARIAS, TODAS? Hay limites?
+                    for i, dic in enumerate(self.WaveData):
+                        if dic['NewData'] == 1:
+                            self.WaveTable.append(self.WaveData[i]['Wave'])
+
+                    print(self.WaveTable)
+                    DiagMatrix = [self.__Cmd_DiagAge[0], self.WaveTable]
+
+                    print("Envio Diag")
+                    self.Diag_Table_input.send(DiagMatrix)
+                    self.__Cmd_DiagAge[0] = 0
+                    print("Fin Envio Diag")
+
+                    self.GuiCurrState = STATE_WAIT_DIAGNOSTIC
+                    print("Waiting Diag Result...")
+                else:
+                    self.GuiCurrState = STATE_STAND_BY
+                    print("Return Stand By")
+
+            elif self.GuiCurrState is STATE_WAIT_RAW_DATA:
+
+                DiagCurrState = self.Ctrl_State[0]
+
+                if DiagCurrState is 0:
+                    self.GuiCurrState = STATE_SEND_RESULT
+
+            elif self.GuiCurrState is STATE_SEND_RESULT:
+
+                print("Inicio Reciv Result")
+                DiagnPEATC = self.Diag_Result_output.recv()
+                print("Fin Reciv Result")
+                print(DiagnPEATC)
+                print("---------")
+                self.GuiCurrState = STATE_STAND_BY
--- Graphic_App/Init_App.py
+++ Graphic_App/Init_App.py
@@ -1,8 +1,4 @@
 from GUI_Control import GUI_Control
-import multiprocessing as mprocess
-from multiprocessing import Array
-from PEATC_Control import PEATC_Control
-from GUI_Control import GUI_Control
 
 if __name__ == '__main__':
     GUI_Control()
