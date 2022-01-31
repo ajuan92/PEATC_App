e0756048100b27eade9b339c09b144c43b8b3c59
Juan Carlos Aguirre López <ajuan.carlos626@gmail.com>
2022-01-30 21:29:38 -0600

    Se agrega archivo Install
    Se actualiza sh para ejecutar la aplicación y se agrega sh para instalar las librerías necesarias para ejecutar la aplicación

--- Init_App.sh
+++ Init_App.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-startx
-sleep 30
 cd /root/Desktop/PEATC_App/Graphic_App/
 python3 Init_App.py
---
+++ Install.sh
@@ -0,0 +1,4 @@
+sudo apt update
+sudo apt install python3.8
+sudo apt install python3-tk
+sudo apt-get install python3-matplotlib
